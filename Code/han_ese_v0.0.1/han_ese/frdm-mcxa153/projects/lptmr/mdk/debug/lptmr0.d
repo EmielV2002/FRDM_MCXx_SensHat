5eb626369a860682ff4ba5d7ed695b39eae1aa27
Emiel V <44370997+EmielV2002@users.noreply.github.com>
2024-06-17 22:05:10 +0200

    Delete 'Code' directory

--- Code/han_ese_v0.0.1/han_ese/frdm-mcxa153/projects/lptmr/mdk/debug/lptmr0.d
+++
@@ -1,9 +0,0 @@
-./debug/lptmr0.o: ..\lptmr0.c ..\lptmr0.h \
-  C:\Users\hugoa\AppData\Local\Arm\Packs\NXP\MCXA153_DFP\17.0.0\devices\MCXA153\MCXA153.h \
-  ..\..\..\..\..\CMSIS\Core\Include\core_cm33.h \
-  C:\Keil_v5\ARM\ARMCLANG\include\stdint.h \
-  C:\Users\hugoa\OneDrive\ -\ HAN\work\frdm-mcxa153\_mic\SDK_2_14_2_FRDM-MCXA153\CMSIS\Core\Include\cmsis_version.h \
-  C:\Users\hugoa\OneDrive\ -\ HAN\work\frdm-mcxa153\_mic\SDK_2_14_2_FRDM-MCXA153\CMSIS\Core\Include\cmsis_compiler.h \
-  C:\Users\hugoa\OneDrive\ -\ HAN\work\frdm-mcxa153\_mic\SDK_2_14_2_FRDM-MCXA153\CMSIS\Core\Include\cmsis_armclang.h \
-  C:\Users\hugoa\AppData\Local\Arm\Packs\NXP\MCXA153_DFP\17.0.0\devices\MCXA153\system_MCXA153.h \
-  C:\Keil_v5\ARM\ARMCLANG\include\stdbool.h
